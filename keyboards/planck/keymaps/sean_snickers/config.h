04787b7740da90ddffb8c210f9e17b6c4c87d9c4
Sean Kegel <me@seankegel.com>
2022-10-10 10:39:52 -0400

    Create sean_snickers Planck layout

---
+++ keyboards/planck/keymaps/sean_snickers/config.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifdef AUDIO_ENABLE
+#    define STARTUP_SONG SONG(PLANCK_SOUND)
+
+#    define DEFAULT_LAYER_SONGS \
+        { SONG(QWERTY_SOUND), SONG(COLEMAK_SOUND), SONG(DVORAK_SOUND) }
+#endif
+
+#define MIDI_BASIC
+
+#define ENCODER_RESOLUTION 4
+
+#define NO_AUTO_SHIFT_ALPHA
