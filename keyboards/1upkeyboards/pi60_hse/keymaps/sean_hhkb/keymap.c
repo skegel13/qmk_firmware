289e83df750977d888212844fd9a24d148f5d007
Sean Kegel <me@seankegel.com>
2022-11-02 12:28:38 -0400

    Update HHKB layout

--- keyboards/1upkeyboards/pi60_hse/keymaps/sean_hhkb/keymap.c
+++ keyboards/1upkeyboards/pi60_hse/keymaps/sean_hhkb/keymap.c
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         ALL_T(KC_TAB),  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
         LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
         KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(1),
-        ALL_T(KC_ESC), KC_LALT, KC_LGUI,                             KC_SPC,                    KC_NO,   KC_RGUI, KC_RALT, MO(2)
+        LT(1, KC_ESC), KC_LALT, KC_LGUI,                             KC_SPC,                    KC_NO,   KC_RGUI, KC_RALT, MO(2)
     ),
 
     [1] = LAYOUT_60_ansi_split_bs_rshift(
