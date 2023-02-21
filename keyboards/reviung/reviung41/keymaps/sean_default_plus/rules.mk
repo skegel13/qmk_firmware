536897e9352b3e0cefd2d5f28211d58a2250b7e6
Sean Kegel <me@seankegel.com>
2023-02-21 15:49:22 -0500

    Create default_plus layout for Reviung

---
+++ keyboards/reviung/reviung41/keymaps/sean_default_plus/rules.mk
@@ -0,0 +1,8 @@
+# MCU name
+MCU = atmega32u4
+BOOTLOADER = atmel-dfu
+MOUSEKEY_ENABLE = yes        # Mouse keys
+CONSOLE_ENABLE = no        # Console for debug
+COMMAND_ENABLE = no        # Commands for debug and configuration
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
