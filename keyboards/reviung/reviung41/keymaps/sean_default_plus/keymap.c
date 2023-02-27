38dc423ba44510da29a1bab6c36807b29d353994
Sean Kegel <me@seankegel.com>
2023-02-27 09:05:19 -0500

    Add sean_reviung_like layout for Iris

--- keyboards/reviung/reviung41/keymaps/sean_default_plus/keymap.c
+++ keyboards/reviung/reviung41/keymaps/sean_default_plus/keymap.c
@@ -37,14 +37,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_LOWER] = LAYOUT(
     _______,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,    KC_PERC,            KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_DEL,
     _______,  KC_UNDS,  KC_PLUS,  KC_LCBR,  KC_RCBR,   KC_PIPE,            KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_GRV,   KC_TILD,
-    _______,  KC_ESC,   KC_LGUI,  KC_LALT,  KC_LCTL,   KC_DQUO,            LGUI(KC_LEFT), LGUI(KC_RGHT), KC_PGUP, KC_PGDN, KC_PSCR, RSFT_T(KC_SPC),
+    _______,  QK_GESC,   KC_LGUI,  KC_LALT,  KC_LCTL,   KC_DQUO,           LGUI(KC_LEFT), KC_PGDN, KC_PGUP, LGUI(KC_RGHT), KC_BSLS, RSFT_T(KC_SPC),
                                             _______,   _______,  KC_ENT,   _______,  _______
   ),
 
   [_RAISE] = LAYOUT(
     _______,  KC_1,     KC_2,     KC_3,     KC_4,      KC_5,               KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_DEL,
     _______,  KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,   KC_BSLS,            KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,
-    _______,  KC_ESC,   KC_RGUI,  KC_RALT,  KC_LCTL,   KC_QUOT,            KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,
+    _______,  QK_GESC,   KC_RGUI,  KC_RALT,  KC_LCTL,   KC_QUOT,           KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,
                                             _______,   _______,  KC_BSPC,  _______,  _______
   ),
 
@@ -59,4 +59,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 layer_state_t layer_state_set_user(layer_state_t state) {
   return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
-
