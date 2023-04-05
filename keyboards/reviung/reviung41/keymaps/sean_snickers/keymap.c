8017fd2602c8e6ee1edb9b21eb116016a5b1a1d8
Sean Kegel <me@seankegel.com>
2023-04-05 13:41:24 -0400

    Update Reviung Snickers layout

--- keyboards/reviung/reviung41/keymaps/sean_snickers/keymap.c
+++ keyboards/reviung/reviung41/keymaps/sean_snickers/keymap.c
@@ -25,14 +25,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_LOWER] = LAYOUT(
     KC_BSPC,       KC_DEL,        SGUI(KC_LBRC), SGUI(KC_RBRC), LGUI(KC_LBRC), LGUI(KC_RBRC),  LCTL(KC_GRV),  KC_LBRC, KC_7,  KC_8,  KC_9,  KC_RBRC,
     KC_LSFT,       KC_LCTL,       KC_LALT,       KC_LGUI,       KC_UP,         KC_PGUP,        OSM(MOD_HYPR), KC_EQL,  KC_4,  KC_5,  KC_6,  KC_SCLN,
-    LGUI(KC_LEFT), LGUI(KC_RGHT), KC_LEFT,       KC_RIGHT,      KC_DOWN,       KC_PGDN,         _______,      KC_BSLS, KC_1,  KC_2,  KC_3,  KC_GRV,
+    LGUI(KC_LEFT), LGUI(KC_RGHT), KC_PGDN,       KC_LEFT,      KC_DOWN,        KC_RGHT,         _______,      KC_BSLS, KC_1,  KC_2,  KC_3,  KC_GRV,
                                             _______,   _______,  KC_ENT,   ADJUST,  KC_0
   ),
 
   [_RAISE] = LAYOUT(
     KC_LCBR,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RCBR,  _______,           SGUI(KC_LBRC), SGUI(KC_RBRC), LGUI(KC_LBRC), LGUI(KC_RBRC), KC_DEL,  KC_BSPC,
-    KC_COLN,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_PLUS,  _______,           KC_WH_U,       KC_MS_U,       KC_LGUI,       KC_LALT,       KC_LCTL, KC_LSFT,
-    KC_TILD,  KC_EXLM,  KC_AT,    KC_HASH,  KC_PIPE,  _______,           KC_WH_D,       KC_MS_D,       KC_MS_L,       KC_MS_R,       KC_BTN1, KC_BTN2,
+    KC_COLN,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_PLUS,  _______,           KC_MS_BTN1,    KC_MS_U,       KC_LGUI,       KC_LALT,       KC_LCTL, KC_LSFT,
+    KC_TILD,  KC_EXLM,  KC_AT,    KC_HASH,  KC_PIPE,  _______,           KC_MS_L,       KC_MS_D,       KC_MS_R,       KC_MS_BTN2,    KC_WH_U, KC_WH_D,
                                             KC_RPRN,  ADJUST,  KC_BSPC,  _______,  _______
   ),
 
