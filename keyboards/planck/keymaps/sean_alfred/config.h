bed05f2b244974446cf0110dba43b5a087dc0325
Sean Kegel <me@seankegel.com>
2023-04-21 14:08:45 -0400

    Update Alfred layouts

--- keyboards/planck/keymaps/sean_alfred/config.h
+++ keyboards/planck/keymaps/sean_alfred/config.h
@@ -33,3 +33,10 @@
 
 // Most tactile encoders have detents every 4 stages
 #define ENCODER_RESOLUTION 4
+
+#define TAPPING_TERM 200
+#define TAPPING_FORCE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT
+#define NO_AUTO_SHIFT_ALPHA
+#define AUTO_SHIFT_TIMEOUT TAPPING_TERM
+#define AUTO_SHIFT_NO_SETUP
