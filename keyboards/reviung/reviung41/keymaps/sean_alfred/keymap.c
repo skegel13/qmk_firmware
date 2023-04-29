8af78995b4b6919be8dbaae2ae176dff251a2da3
Sean Kegel <me@seankegel.com>
2023-04-29 14:09:38 -0400

    Add Alfred layout for Iris

--- keyboards/reviung/reviung41/keymaps/sean_alfred/keymap.c
+++ keyboards/reviung/reviung41/keymaps/sean_alfred/keymap.c
@@ -1,18 +1,3 @@
-/* Copyright 2020 gtips
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 #include QMK_KEYBOARD_H
 
 enum layer_names {
@@ -34,21 +19,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LSFT,        KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,               KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  RSFT_T(KC_ENT),
                                             KC_LGUI,   LOWER,    NUMMOUSE, RAISE,    KC_RALT
   ),
-  
+
   [_LOWER] = LAYOUT(
     _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,            SGUI(KC_LBRC), SGUI(KC_RBRC), LGUI(KC_LBRC), LGUI(KC_RBRC), XXXXXXX, KC_DEL,
     _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_UP,     XXXXXXX,            KC_LEFT,       KC_DOWN,       KC_UP,         KC_RGHT,       XXXXXXX, XXXXXXX,
     _______,  XXXXXXX,  XXXXXXX,  KC_LEFT,  KC_DOWN,   KC_RGHT,            LGUI(KC_LEFT), KC_PGDN,       KC_PGUP,       LGUI(KC_RGHT), XXXXXXX, _______,
                                             _______,   _______,  KC_ENT,   _______,  _______
   ),
-  
+
   [_RAISE] = LAYOUT(
     KC_TILD,  KC_EXLM,      KC_AT,        KC_HASH,      KC_DLR,            KC_PERC,            KC_CIRC,  KC_AMPR,      KC_ASTR,      KC_LPRN,      KC_RPRN,      KC_PIPE,
     KC_GRV,   LGUI_T(KC_1), LALT_T(KC_2), LCTL_T(KC_3), LSFT_T(KC_4),      KC_5,               KC_6,     LSFT_T(KC_7), LCTL_T(KC_8), LALT_T(KC_9), LGUI_T(KC_0), KC_BSLS,
     _______,  KC_MINS,      KC_UNDS,      KC_LPRN,      KC_LBRC,           KC_LCBR,            KC_RCBR,  KC_RBRC,      KC_RPRN,      KC_EQL,       KC_PLUS,      _______,
                                             _______,   _______,  KC_BSPC,  _______,  _______
   ),
-  
+
   [_ADJUST] = LAYOUT(
     RGB_TOG, RGB_MOD, RGB_VAI, RGB_SAI, RGB_HUI, RGB_SPI,            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  QK_BOOT,
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,              XXXXXXX,  KC_LSFT,  KC_LCTL,  KC_LALT,  KC_LGUI,  XXXXXXX,
