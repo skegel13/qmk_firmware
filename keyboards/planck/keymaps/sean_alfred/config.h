655acbfe7196fa1b9b8af08c3f42a16f32f614bf
Sean Kegel <me@seankegel.com>
2023-06-02 11:32:26 -0400

    Update Planck Alfred and Iris Koda Rev layouts

--- keyboards/planck/keymaps/sean_alfred/config.h
+++ keyboards/planck/keymaps/sean_alfred/config.h
@@ -4,39 +4,17 @@
 
 #ifdef AUDIO_ENABLE
 #    define STARTUP_SONG SONG(PLANCK_SOUND)
-// #define STARTUP_SONG SONG(NO_SOUND)
-
 #    define DEFAULT_LAYER_SONGS \
         { SONG(QWERTY_SOUND), SONG(COLEMAK_SOUND), SONG(DVORAK_SOUND) }
 #endif
 
-/*
- * MIDI options
- */
-
-/* enable basic MIDI features:
-   - MIDI notes can be sent when in Music mode is on
-*/
-
 #define MIDI_BASIC
 
-/* enable advanced MIDI features:
-   - MIDI notes can be added to the keymap
-   - Octave shift and transpose
-   - Virtual sustain, portamento, and modulation wheel
-   - etc.
-*/
-//#define MIDI_ADVANCED
-
-/* override number of MIDI tone keycodes (each octave adds 12 keycodes and allocates 12 bytes) */
-//#define MIDI_TONE_KEYCODE_OCTAVES 2
-
-// Most tactile encoders have detents every 4 stages
 #define ENCODER_RESOLUTION 4
 
-#define TAPPING_TERM 200
+#define TAPPING_TERM 175
 #define TAPPING_FORCE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
 #define NO_AUTO_SHIFT_ALPHA
-#define AUTO_SHIFT_TIMEOUT TAPPING_TERM
+#define AUTO_SHIFT_TIMEOUT 150
 #define AUTO_SHIFT_NO_SETUP
