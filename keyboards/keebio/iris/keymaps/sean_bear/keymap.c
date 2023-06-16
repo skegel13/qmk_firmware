228b0a1392373fa03c151d65ca242c0f44fe5bd0
Sean Kegel <me@seankegel.com>
2023-06-16 10:29:03 -0400

    Update Bear layouts

--- keyboards/keebio/iris/keymaps/sean_bear/keymap.c
+++ keyboards/keebio/iris/keymaps/sean_bear/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, KC_WH_D, KC_MS_U, KC_WH_U, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, KC_MINS, KC_EQL,  KC_DEL,
+     _______, KC_WH_D, KC_MS_U, KC_WH_U, XXXXXXX, XXXXXXX,                            KC_BTN3, KC_BTN1, KC_BTN2, KC_MINS, KC_EQL,  KC_DEL,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_UP,   XXXXXXX,                            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
