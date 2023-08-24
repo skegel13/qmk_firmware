be1a45e2a553c54be80e0f803d219b9da951db53
Sean Kegel <me@seankegel.com>
2023-08-23 20:22:52 -0400

    Update Bear layout for Planck

--- keyboards/planck/keymaps/sean_bear/keymap.c
+++ keyboards/planck/keymaps/sean_bear/keymap.c
@@ -11,24 +11,23 @@ enum planck_layers {
   _ADJUST
 };
 
-#define LOWER MO(_LOWER)
-#define RAISE MO(_RAISE)
-#define NUMBER LT(_NUMBER, KC_SPC)
+#define LOWER LT(_LOWER, KC_BSPC)
+#define RAISE LT(_RAISE, KC_TAB)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  // XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
 
 [_QWERTY] = LAYOUT_planck_grid(
-    ALL_T(KC_TAB), KC_Q, KC_W, KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+    ALL_T(KC_GRV), KC_Q, KC_W, KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
     LCTL_T(KC_ESC), KC_A, KC_S, KC_D,   KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SC_SENT,
     ALL_T(KC_ESC), KC_LCTL, KC_LALT, KC_LGUI, LOWER, KC_SPC, KC_SPC, RAISE, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 [_LOWER] = LAYOUT_planck_grid(
-    _______, KC_WH_D, KC_MS_U, KC_WH_U, _______, _______, KC_BTN3, KC_BTN1, KC_BTN2, KC_MINS, KC_EQL,  KC_DEL,
-    _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_UP,   _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
+    _______, KC_WH_D, KC_MS_U, KC_WH_U, _______, _______, KC_BTN3, KC_BTN1, KC_BTN2, KC_MINS, KC_EQL,  KC_BSPC,
+    _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_UP,   _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_DEL,
     _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, KC_BTN1, KC_BTN2, _______, _______
 ),
